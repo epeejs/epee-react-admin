6f4232ba28ce3bb91c858037e5cebf51f341d863
zhangjiawei <1103650146@qq.com>
2019-09-12 12:35:45 +0800

    add redux-logger

--- src/constants/store.ts
+++ src/constants/store.ts
@@ -1,4 +1,14 @@
 import { createStore } from 'easy-peasy';
 import { storeModel } from 'models';
+import { Middleware } from 'redux';
+import logger from 'redux-logger';
 
-export const store = createStore(storeModel);
+const middlewares: Middleware[] = [];
+
+if (process.env.NODE_ENV === 'development' || window.config.debug) {
+  middlewares.push(logger);
+}
+
+export const store = createStore(storeModel, {
+  middleware: middlewares,
+});
