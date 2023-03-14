5041d18a2b1eec832517df8cb12b116afde0a989
zhangjw05 <zhangjw05@mingyuanyun.com>
2023-03-14 19:33:45 +0800

    chore: 支持lodash按需引入及构建分析

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import react from '@vitejs/plugin-react';
+import visualizer from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['lodash'],
+      },
+    }),
+    process.env.ANALYZE
+      ? visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+        })
+      : undefined,
+  ],
   resolve: {
     alias: [{ find: /^src(.*)/, replacement: `${__dirname}/src$1` }],
   },
